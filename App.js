f45cbf26aa35804bde93769f2799c206c571c023
Bing Z <bingz@me.com>
2021-02-23 12:20:35 -0700

    Render text (model, price.)

--- App.js
+++ App.js
@@ -5,8 +5,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <View style={styles.carContainer}>
+            <View style={styles.titles}>
+            <Text style={styles.title}> Model S</Text>  
+            <Text style={styles.subTitle}> Starting at $69,420</Text>  
+            </View>
+        </View> 
+        <StatusBar style="auto" />
     </View>
   );
 }
@@ -18,4 +23,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  carContainer: {
+    width: '100%',
+    height: '100%',
+
+  },
+  titles: {
+    marginTop: '30%',
+    width: '100%',
+    alignItems: 'center'
+  },
+  title:{
+    fontSize: 40,
+    fontWeight: '500'
+  },
+  subTitle: {
+    fontSize: 16,
+    color: '#5c5e62'
+  }
 });
