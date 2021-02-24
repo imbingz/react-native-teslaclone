ffb5001cbec3bdb0541309e6139e09dee1a3299c
Bing Z <bingz@me.com>
2021-02-24 11:07:08 -0700

    added button component, styles and props

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { Text, View, ImageBackground } from 'react-native';
+import StyledButton from '../StyledButton';
 import styles from './styles';
-
-
 
 const CarItem = (props) => {
     return (
@@ -12,9 +11,19 @@ const CarItem = (props) => {
                 style={styles.image} 
             />
             <View style={styles.titles}>
-            <Text style={styles.title}> Model S</Text>  
-            <Text style={styles.subTitle}> Starting at $69,420</Text>  
+                <Text style={styles.title}> Model S</Text>  
+                <Text style={styles.subTitle}> Starting at $69,420</Text>  
             </View>
+            <StyledButton 
+                type='primary' 
+                content='Custom Order'
+                onPress={() => console.warn('Custom Order was pressed')}
+            />
+            <StyledButton 
+                type='secondary' 
+                content='Existing Inventory'
+                onPress={() => console.warn('Existing Inventory was pressed')}
+            />
         </View> 
     )
 }
